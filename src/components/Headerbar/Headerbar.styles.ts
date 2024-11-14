3c70c44418916dfce5e1033b211894dcb07c19ed
Théo Barreto Silva <barretotheo25@gmail.com>
2024-11-14 20:15:08 -0300

    headerbar e funcalmoxlayout pronto

---
+++ src/components/Headerbar/Headerbar.styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const HeaderStyled = styled.header`
+    width: 100vw;
+    height: 15vh;
+    background-color: #90C9D3;
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 3px 8px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
+
+export const ImgLogo = styled.img`
+    height: 10vh;
+    object-fit: cover;
+`
