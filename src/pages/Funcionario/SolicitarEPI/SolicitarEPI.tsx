ec9060a33087b184aea468a22f11e6115377ddb7
Théo Barreto Silva <barretotheo25@gmail.com>
2024-11-15 23:30:48 -0300

    fazendo tabela

--- src/pages/Funcionario/SolicitarEPI/SolicitarEPI.tsx
+++ src/pages/Funcionario/SolicitarEPI/SolicitarEPI.tsx
@@ -3,10 +3,42 @@ import { Paper } from '@mui/material'
 // import { InputStyled } from '../../../components/InputStyled/InputStyled'
 import * as S from './SolicitarEPI.styles'
 import { Searchbar } from '../../../components/Searchbar/Searchbar'
-// import { DataGrid } from '@mui/x-data-grid'
+import { DataGrid, GridActionsCellItem, GridColDef, GridRowParams } from '@mui/x-data-grid'
+import { OpenModalIcon } from '../../../components/OpenModalIcon/OpenModalIcon'
 
 export const SolicitarEPI = () => {
-    // const paginationModel = { page: 0, pageSize: 5 };
+    const paginationModel = { page: 0, pageSize: 5 };
+    const columns: GridColDef[] = [
+        {
+            field: 'open',
+            type: 'actions',
+            headerName: 'Abrir',
+            getActions: (params: GridRowParams) => [
+                <GridActionsCellItem 
+                    key={0}
+                    icon={
+                        <OpenModalIcon />
+                    }
+                    label='open'
+                />
+            ],
+        },
+        {field: 'id', headerName: 'ID', width: 200, align: 'center'},
+        {field: 'descricaoItem', headerName: 'Descrição de Item', width: 200, align: 'center'},
+        {field: 'status', headerName: 'Status', width: 200, align: 'center'}
+    ];
+    const rows = [
+        { id: 'SOL-24-01', descricaoItem: 'Abafadores de som', status: 'Pendente'},
+        { id: 'SOL-24-01', descricaoItem: 'Abafadores de som', status: 'Pendente',},
+        { id: 'SOL-24-01', descricaoItem: 'Abafadores de som', status: 'Pendente',},
+        { id: 'SOL-24-01', descricaoItem: 'Abafadores de som', status: 'Pendente',},
+        { id: 'SOL-24-01', descricaoItem: 'Abafadores de som', status: 'Pendente',},
+        { id: 'SOL-24-01', descricaoItem: 'Abafadores de som', status: 'Pendente',},
+        { id: 'SOL-24-01', descricaoItem: 'Abafadores de som', status: 'Pendente',},
+        { id: 'SOL-24-01', descricaoItem: 'Abafadores de som', status: 'Pendente',},
+        { id: 'SOL-24-01', descricaoItem: 'Abafadores de som', status: 'Pendente',},
+    ];
+
 
     return(
         <S.MainStyled>
@@ -20,17 +52,17 @@ export const SolicitarEPI = () => {
                 <br />
                 <BtnStyled text='Solicitar' />
             </S.BoxSolEPI> */}
-            <S.BoxSolicitacoes>
+            {/* <S.BoxSolicitacoes> */}
                 <Paper sx={{ height: '100%', width: '100%' }}>
-                    {/* <DataGrid
-                        rows={}
-                        columns={}
+                    <DataGrid
+                        rows={rows}
+                        columns={columns}
                         initialState={{ pagination: { paginationModel } }}
                         pageSizeOptions={[5, 10]}
                         sx={{ border: 0 }}
-                    /> */}
+                    />
                 </Paper>
-            </S.BoxSolicitacoes>
+            {/* </S.BoxSolicitacoes> */}
         </S.MainStyled>
     )
 }
