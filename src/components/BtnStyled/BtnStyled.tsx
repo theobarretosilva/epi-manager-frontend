23ee688e17b9d9c7c20020903e6eae90c6af01d3
DudaFranderlinde <108769434+DudaFranderlinde@users.noreply.github.com>
2024-11-20 19:22:49 -0300

    Ajustando props

--- src/components/BtnStyled/BtnStyled.tsx
+++ src/components/BtnStyled/BtnStyled.tsx
@@ -2,7 +2,7 @@ import * as S from './BtnStyled.styles'
 
 interface BtnStyledProps {
     text: string;
-    type: "button" | "submit" | "reset";
+    type?: "button" | "submit" | "reset";
 }
 
 export const BtnStyled: React.FC<BtnStyledProps> = ({ text, type }) => {
