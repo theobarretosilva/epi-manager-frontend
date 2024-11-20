ad52f285f3c2d95c322e7c4e4697d8c406a9c326
DudaFranderlinde <108769434+DudaFranderlinde@users.noreply.github.com>
2024-11-20 19:22:18 -0300

    Add modal a div root

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { BrowserRouter } from 'react-router-dom'
 import './App.css'
 import { Router } from './router/Routes'
+import ReactModal from 'react-modal';
 
 function App() {
+  ReactModal.setAppElement('#root');
   return (
     <BrowserRouter>
       <Router />
