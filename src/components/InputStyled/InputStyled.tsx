7282fafc9c64f1fa46f46b7964261c77c5784fce
Théo Barreto Silva <barretotheo25@gmail.com>
2024-11-19 21:50:32 -0300

    finalizando design tela de solicitar EPI

--- src/components/InputStyled/InputStyled.tsx
+++ src/components/InputStyled/InputStyled.tsx
@@ -3,14 +3,16 @@ import * as S from './InputStyled.styles'
 interface InputStyledProps {
     titulo: string;
     tipo: string;
-    placeholder: string;
+    placeholder?: string;
+    value?: string;
+    disabled?: boolean;
 }
 
-export const InputStyled = ({ titulo, tipo, placeholder }: InputStyledProps) => {
+export const InputStyled = ({ titulo, tipo, placeholder, value, disabled }: InputStyledProps) => {
     return (
         <S.DivGeral>
             <S.NameInput>{titulo}</S.NameInput>
-            <S.InputStyled type={tipo} placeholder={placeholder} />
+            <S.InputStyled value={value} type={tipo} placeholder={placeholder} disabled={disabled} />
         </S.DivGeral>
     );
 };
--- src/pages/Funcionario/SolicitarEPI/SolicitarEPI.tsx
+++ src/pages/Funcionario/SolicitarEPI/SolicitarEPI.tsx
@@ -1,9 +1,78 @@
+import { useState } from 'react'
+import { BtnStyled } from '../../../components/BtnStyled/BtnStyled'
+import { InputStyled } from '../../../components/InputStyled/InputStyled'
+import { SelectCodStyled } from '../../../components/SelectCodStyled/SelectCodStyled'
 import * as S from './SolicitarEPI.styles'
+import { SelectStyled } from '../../../components/SelectStyled/SelectStyled'
 
 export const SolicitarEPI = () => {
+    const [selectedOption, setSelectedOption] = useState({ label: '', value: '' });
+    const [codigo, setCodigo] = useState('');
+    const handleSelectChange = (option: { label: string; value: string }) => {
+        console.log('Selecionado:', option);
+        setSelectedOption(option);
+        setCodigo(option.value);
+    };
+
+    const [selectedPrioridade, setSelectedPrioridade] = useState('');
+    const handleSelectPrioridadeChange = (value: string) => {
+        console.log('Selecionado:', value);
+        setSelectedPrioridade(value);
+    };
+
+    const options = [
+        { label: 'Capacete de proteção', value: 'COD-01' },
+        { label: 'Óculos de proteção', value: 'COD-02' },
+        { label: 'Luva de borracha', value: 'COD-03' }
+    ];
+
     return(
         <S.MainStyled>
-            main solicitar epi
+            <S.DivMainSolicitar>
+                <S.DivFlex>
+                    <InputStyled 
+                        disabled={true}
+                        tipo='text'
+                        titulo='ID da Solicitação'
+                        value='SOL-24-1'
+                    />
+                    <InputStyled 
+                        disabled={true}
+                        tipo='text'
+                        titulo='Solicitante'
+                        value='Felipe Soares de Oliveira'
+                    />
+                </S.DivFlex>
+                <S.DivFlex>
+                    <SelectCodStyled 
+                        titulo="Escolha um item"
+                        value={selectedOption.value}
+                        options={options}
+                        onChange={handleSelectChange}
+                    />
+                    <InputStyled 
+                        tipo='text'
+                        titulo='Código'
+                        disabled={true}
+                        value={codigo}
+                    />
+                </S.DivFlex>
+                <S.DivFlex>
+                    <SelectStyled
+                        titulo="Prioridade" 
+                        value={selectedPrioridade} 
+                        options={['Alta', 'Média', 'Baixa']} 
+                        onChange={handleSelectPrioridadeChange} 
+                    />
+                    <InputStyled 
+                        tipo='number'
+                        titulo='Quantidade'
+                    />
+                </S.DivFlex>
+                <br />
+                <BtnStyled text='Solicitar' />
+                <S.PVoltar>Voltar</S.PVoltar>
+            </S.DivMainSolicitar>
         </S.MainStyled>
     )
 }
