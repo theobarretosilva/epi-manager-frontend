31de7a383bfbef7271c56550e2f295bc1b746ff8
DudaFranderlinde <108769434+DudaFranderlinde@users.noreply.github.com>
2024-11-20 16:21:21 -0300

    Add propriedades

--- src/components/InputStyled/InputStyled.tsx
+++ src/components/InputStyled/InputStyled.tsx
@@ -3,14 +3,17 @@ import * as S from './InputStyled.styles'
 interface InputStyledProps {
     titulo: string;
     tipo: string;
-    placeholder: string;
+    placeholder?: string;
+    value?: string;
+    name?: string;
+    handle?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export const InputStyled = ({ titulo, tipo, placeholder }: InputStyledProps) => {
+export const InputStyled = ({ titulo, tipo, placeholder, name, value, handle}: InputStyledProps) => {
     return (
         <S.DivGeral>
             <S.NameInput>{titulo}</S.NameInput>
-            <S.InputStyled type={tipo} placeholder={placeholder} />
+            <S.InputStyled name={name}  type={tipo} placeholder={placeholder} value={value} onChange={handle} />
         </S.DivGeral>
     );
 };
