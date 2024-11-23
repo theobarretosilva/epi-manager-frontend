9a949bed0dc07e2d6fa58aaae150012dea57bcbd
Théo Barreto Silva <barretotheo25@gmail.com>
2024-11-23 00:55:30 -0300

    cadastrando userMaster quando iniciar aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,33 @@ import { BrowserRouter } from 'react-router-dom'
 import './App.css'
 import { Router } from './router/Routes'
 import ReactModal from 'react-modal';
+import { useEffect } from 'react';
 
 function App() {
   ReactModal.setAppElement('#root');
+  
+  useEffect(() => {
+    const defaultAdmin = {
+      matricula: '01',
+      salt: '861918edf24cc72e2536a8b254a0f692',
+      hash: 'af067cef1a2dd778c535f67aa3acde28c8b33db518b3d8a384943f6069c3f648',
+      tipoAcesso: 'Administrador',
+      cargo: 'Administrador',
+      email: 'admin_master@gmail.com',
+      id: '01',
+      nome: 'Administrador Master',
+      setor: 'Soldagem'
+    };
+
+    const colaboradores = JSON.parse(sessionStorage.getItem('ColaboradoresCadastrados') || '[]');
+
+    if (!colaboradores.some((colaborador) => colaborador.matricula === defaultAdmin.matricula)) {
+      sessionStorage.setItem(
+        'ColaboradoresCadastrados',
+        JSON.stringify([...colaboradores, defaultAdmin])
+      );
+    }
+  }, []);
   return (
     <BrowserRouter>
       <Router />
