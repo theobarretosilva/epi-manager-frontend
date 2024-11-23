fa007172062faa75f306ac45d212a0434ffac447
Théo Barreto Silva <barretotheo25@gmail.com>
2024-11-22 22:53:23 -0300

    rota protegida implementada

--- src/router/Routes.tsx
+++ src/router/Routes.tsx
@@ -13,28 +13,30 @@ import { Solicitacoes } from "../pages/Administrador/Solicitacoes/Solicitacoes";
 import { ConsultColab } from "../pages/Almoxarifado/ConsultColab/ConsultColab";
 import { ConsultEPI } from "../pages/Almoxarifado/ConsultEPI/ConsultEPI";
 import { Usuarios } from "../pages/Usuarios/Usuarios";
+import { ProtectedRoute } from "./ProtectedRoute";
 
 export const Router = () => {
     return(
         <Routes>
             <Route index path="/" element={<Usuarios/>} />
             <Route path="/login" element={<Login/>} />
-            <Route path="/funcionario" element={<FuncLayout />}>
+            <Route path="/funcionario" element={<ProtectedRoute element={<FuncLayout />} roles={['Colaborador']} />}>
                 <Route path="solicitacoes" element={<Solicitacoes />} />
                 <Route path="solicitarEPI" element={<SolicitarEPI />} />
             </Route>
-            <Route path="/almoxarifado" element={<AlmoxLayout />} >
+            <Route path="/almoxarifado" element={<ProtectedRoute element={<AlmoxLayout />} roles={['Almoxarifado']} />} >
                 <Route path="dashboardAlmox" element={<DashboardAlmox />} />
                 <Route path="consultColab" element={<ConsultColab />} />
                 <Route path="consultEPI" element={<ConsultEPI />} />
             </Route>
-            <Route path="/administrador" element={<AdminLayout />} >
+            <Route path="/administrador" element={<ProtectedRoute element={<AdminLayout />} roles={['Administrador']} />} >
                 <Route path="solicitacoes" element={<Solicitacoes />} />
                 <Route path="dashboardEPI" element={<DashboardEPI />} />
                 <Route path="dashboardFuncionario" element={<DashboardColab />} />
                 <Route path="cadastroColab" element={<CadastroColaborador />} />
                 <Route path="cadastroEPI" element={<CadastroEPI />} />
             </Route>
+            <Route path="*" element={<h1>404 - Página Não Encontrada</h1>} />
         </Routes>
     )
 }
